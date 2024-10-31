7d1b7497322f13637bb43ef4faf63c3c6c36937b
DnG-Crafts <140936256+DnG-Crafts@users.noreply.github.com>
2024-10-31 10:02:20 +0800

    Update run_sequence.sh
    "fpp -C" was depreciated, REST api now used.

--- FPP Script/run_sequence.sh
+++ FPP Script/run_sequence.sh
@@ -18,16 +18,16 @@ BASE=$(basename $0)
 SPID=$(ps -edaf | grep ${BASE} | grep -v grep | grep -v eventScript | grep -v " $$ " | awk '{print $2}')
 if [ -n "${SPID}" ]; then
     kill ${SPID}
-    fpp -C "Effects Stop"
+	curl "http://127.0.0.1/api/command/Effects%20Stop"
 fi
 fpp -G ${RGPIO},Output
 fpp -G ${LGPIO},Output
 fpp -g ${RGPIO},Output,1
 fpp -g ${LGPIO},Output,1
-fpp -C "All Lights Off"
-fpp -C "FSEQ Effect Start" "$1"
+curl "http://127.0.0.1/api/command/All%20Lights%20Off"
+curl "http://127.0.0.1/api/command/FSEQ%20Effect%20Start/$1"
 coproc read -t "${RT}" && wait "$!" || true
-fpp -C "Effects Stop"
-fpp -C "All Lights Off"
+curl "http://127.0.0.1/api/command/Effects%20Stop"
+curl "http://127.0.0.1/api/command/All%20Lights%20Off"
 fpp -g ${RGPIO},Output,0
 fpp -g ${LGPIO},Output,0
